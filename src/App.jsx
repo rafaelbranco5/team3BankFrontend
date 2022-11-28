2ae93724c22569bb4412ad75dd39dac9ab143d99
Rafael Branco <Rafael.Branco@solera.com>
2022-11-28 09:54:42 +0000

    Routes and LogIn chages

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
 import { useState } from 'react'
 import './App.css'
 import Layout from './Layout'
+import Login from './pages/Login'
+import Signup from './pages/SignUp'
+import Navbar from './Compontents/Navbar'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
+  /*  <div className="App">
       <Layout>
       </Layout>
     </div>
+    */
+
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    </Router>
   )
 }
 
--- src/Compontents/UserDataForm.jsx
+++ src/Compontents/UserDataForm.jsx
@@ -1,16 +1,39 @@
 import React from 'react'
 import styled from 'styled-components'
+import './styles.css'
 
-const UserDataFormContainer = styled.form`
-
+/*
+const UserDataFormContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  
+  input{
+    position: relative;
+    width: 60%;
+    align-items: center;
+    justify-content: center;
+  }
 `
-
+*/
 function UserDataForm() {
   return (
-    <UserDataFormContainer>
-        <input type="text" />
-
-    </UserDataFormContainer>
+    <div className='userform'>
+    <form>
+    <div className='inputBox'>
+      <input type="text" required/>
+      <span>Username</span>
+    </div>
+    <div className='inputBox'>
+      <input type="password" required/>
+      <span>Password</span>
+    </div>
+      <input type="submit" />
+    </form>
+    </div>
   )
 }
 
